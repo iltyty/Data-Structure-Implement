cb657e0a9e5762ad2ef4758eeb8616bd6c06950c
QiuYuanhui <qiuyh0924@163.com>
2020-02-06 09:38:37 +0800

    feat(string): finish string ds test

--- string/test.cpp
+++ string/test.cpp
@@ -46,22 +46,23 @@ void test_string(int size) {
 }
 
 void test_operator_add(String &str, int size) {
-    cout << "test1 operator +=" << endl;
-    cout << "before add: " << str << "after add ";
+    cout << endl << "test1 operator +=" << endl;
+    cout << "before add: " << str;
     for (int i = 0; i < size; i++) {
         int r = rand() % LEN;
-        cout << alphabet[r];
         str += alphabet[r];
     }
-    cout << ": " << endl <<  str << endl;
+    cout << "after add: " << endl <<  str << endl;
 }
 
 void test_substr(String &str) {
     String sub1 = str.substr(0, 5);
     String sub2 = str.substr(5, 4);
+    String sub3 = str.substr(0, str.length());
     cout << "test2 substring" << endl;
-    cout << "substr(0, 5): " << sub1;
-    cout << "substr(5, 4): " << sub2 << endl;
+    cout << "substr(0, 5): " << endl << sub1;
+    cout << "substr(5, 4): " << endl << sub2;
+    cout << "substr(0, " << str.length() << "): " << endl << sub3 << endl;
 }
 
 void test_prefix(String &str) {
@@ -70,10 +71,10 @@ void test_prefix(String &str) {
     String pre3 = str.prefix(10);
     String pre4 = str.prefix(str.length());
     cout << "test3 prefix" << endl;
-    cout << "prefix(0)" << pre1;
-    cout << "prefix(5)" << pre2;
-    cout << "prefix(10)" << pre3;
-    cout << "prefix(" << str.length() << ")" << pre3 << endl;
+    cout << "prefix(0): " << endl << pre1;
+    cout << "prefix(5): " << endl << pre2;
+    cout << "prefix(10): " << endl << pre3;
+    cout << "prefix(" << str.length() << "): " << endl << pre4 << endl;
 }
 
 void test_suffix(String &str) {
@@ -82,10 +83,10 @@ void test_suffix(String &str) {
     String suf3 = str.suffix(10);
     String suf4 = str.suffix(str.length());
     cout << "test4 suffix" << endl;
-    cout << "suffix(0)" << suf1;
-    cout << "suffix(5)" << suf2;
-    cout << "suffix(10)" << suf3;
-    cout << "suffix(" << str.length() << ")" << suf3 << endl;
+    cout << "suffix(0):" << endl << suf1;
+    cout << "suffix(5):" << endl << suf2;
+    cout << "suffix(10):" << endl << suf3;
+    cout << "suffix(" << str.length() << "):" << endl << suf4 << endl;
 }
 
 void test_concat(String &str) {
@@ -93,9 +94,9 @@ void test_concat(String &str) {
     String str2("123", 3);
     cout << "test5 concat" << endl;
     str.concat(str1);
-    cout << "concat \"\"(empty string): " << str;
+    cout << "concat \"\"(empty string): " << endl << str;
     str.concat(str2);
-    cout << "concat \"123\": " << str;
+    cout << "concat \"123\": " << endl << str << endl;
 }
 
 void test_equal(String &str) {
@@ -104,6 +105,6 @@ void test_equal(String &str) {
     bool flag1 = str.equal(str1);
     bool flag2 = str.equal(str2);
     cout << "test6 equal" << endl;
-    cout << str << "==" << str1 << ": " << (flag1 ? "true" : "false");
-    cout << str << "==" << str2 << ": " << (flag2 ? "true" : "false");
+    cout << str << "||" << endl << str1 << ": " << (flag1 ? "true" : "false") << endl;
+    cout << str << "==" << str2 << ": " << (flag2 ? "true" : "false") << endl;
 }
